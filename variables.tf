e8d944df3ac51949b621761e04bd364e60996c8e
mbageri <mohammadbageri@outlook.com>
2023-10-23 00:30:15 +0200

    add files

---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "K8S_WORKER_COUNT" {}
+variable "K8S_WORKER_FLAVOR {}
+variable "K8S_IMAGE_NAME" {}
+variable "K8S_NETWORK_NAME" {}
+variable "K8S_KEY_PAIR" {}
+variable "K8S_KEY_PAIR_LOCATION" {}
+variable "K8S_SECURITY_GROUP" {}
